2a16e6a11095e0e928161e1c1a19ec49ec2317ee
Vincent Faust <vincentffaust@gmail.com>
2023-07-18 15:06:16 -0500

    implement parent class

--- src/extract/orders.py
+++ src/extract/orders.py
@@ -1,23 +1,11 @@
-import requests
+from utility import Crypto
 
 
-class Orders:
-    def __init__(self, base_url, endpoint, params):
-        self.base_url = base_url
-        self.cursor = ""
-        self.endpoint = endpoint
+class Orders(Crypto):
+    def __init__(self, parameters):
+        self.endpoint = "orders"
         self.orders = []
-        self.params = params
-
-    def get_main_session(self):
-        session = requests.get(
-            f"{self.base_url}{self.endpoint}?cursor={self.cursor}", params=self.params
-        )
-        session.raise_for_status()
-        return session.json()
-
-    def cursor_helper(self):
-        pass
+        super().__init__(parameters)
 
     def json_elements(self, data):
         for element in data["result"]:
@@ -45,14 +33,14 @@ class Orders:
         return self.orders
 
 
-query_params = {
-    "sell_token_address": "0xacb3c6a43d15b907e8433077b6d38ae40936fe2c",
-    "min_timestamp": "2023-07-18T00:00:00.00Z",
-}
+gods_orders = Orders(
+    parameters=(
+        {
+            "sell_token_address": "0xacb3c6a43d15b907e8433077b6d38ae40936fe2c",
+            "min_timestamp": "2023-07-18T00:00:00.00Z",
+            "max_timestamp": "2023-07-18T00:59:59.99Z",
+        }
+    ),
+)
 
-gods_orders = Orders("https://api.x.immutable.com/v3/", "orders", params=query_params)
-
-data = gods_orders.get_main_session()
-
-results = gods_orders.json_elements(data)
-print(results)
+print(gods_orders.orders)
---
+++ src/extract/utility.py
@@ -0,0 +1,28 @@
+import requests
+
+
+class Crypto:
+    def __init__(self, params):
+        self.params = params
+        self.base_url = "https://api.x.immutable.com/v3/"
+        self.cursor = ""
+
+        while True:
+            data = self.get_main_session()
+            if data["remaining"] == 1:
+                self.get_main_session()
+                self.json_elements(data)
+                self.cursor = self.cursor_helper(data)
+
+            else:
+                break
+
+    def get_main_session(self):
+        session = requests.get(
+            f"{self.base_url}{self.endpoint}?cursor={self.cursor}", params=self.params
+        )
+        session.raise_for_status()
+        return session.json()
+
+    def cursor_helper(self, data):
+        return data["cursor"]
