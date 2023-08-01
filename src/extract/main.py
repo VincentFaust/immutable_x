b42621a106f412b608577864de2bd9ed796d9dd2
Vincent Faust <vincentffaust@gmail.com>
2023-08-01 11:32:51 -0500

    add logging

--- src/extract/main.py
+++ src/extract/main.py
@@ -2,39 +2,53 @@ from mints import Mint
 from orders import Order
 from transfers import Transfer
 
+import logging
 
-def main():
+
+def pipeline():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[%(levelname)s][%(asctime)s][%(filename)s]: %(message)s",
+    )
+
+    logging.info("orders endpoint extraction start")
     gods_orders = Order(
         parameters=(
             {
                 "sell_token_address": "0xacb3c6a43d15b907e8433077b6d38ae40936fe2c",
                 "min_timestamp": "2023-07-18T00:00:00.00Z",
-                "max_timestamp": "2023-07-18T00:59:59.99Z",
+                "max_timestamp": "2023-07-18T00:30:59.99Z",
             }
         ),
     )
+    logging.info("orders endpoint extraction complete")
 
+    logging.info("mints endpoint extraction start")
     gods_mints = Mint(
         parameters=(
             {
                 "token_address": "0xacb3c6a43d15b907e8433077b6d38ae40936fe2c",
                 "min_timestamp": "2023-07-18T00:00:00.00Z",
-                "max_timestamp": "2023-07-18T01:59:59.99Z",
+                "max_timestamp": "2023-07-18T00:30:59.99Z",
                 "status": "success",
             }
         ),
     )
+    logging.info("mints endpoint extraction complete")
 
+    logging.info("transfers endpoint extraction start")
     gods_transfers = Transfer(
         parameters=(
             {
                 "token_address": "0xacb3c6a43d15b907e8433077b6d38ae40936fe2c",
                 "min_timestamp": "2023-07-18T00:00:00.00Z",
-                "max_timestamp": "2023-07-18T01:59:59.99Z",
+                "max_timestamp": "2023-07-18T00:30:59.99Z",
                 "status": "success",
             }
         ),
     )
+
+    logging.info("transfers endpoint extraction complete")
 
     print(gods_orders.orders)
     print(gods_mints.mints)
@@ -42,4 +56,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    pipeline()
