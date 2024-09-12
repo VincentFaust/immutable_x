82fdb6e7faadfa224c181bfa1958dab13174860b
Vincent Faust <vincentffaust@gmail.com>
2024-09-11 19:36:04 -0500

    remove kafka int

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # immutable_x
 
-1. go through kafka lectures 
-2. get script running
-3. pytests
-4. connecting script to kafka
--- kafka-python/config.py
+++
@@ -1,7 +0,0 @@
-config = {
-    "bootstrap.servers": "pkc-ymrq7.us-east-2.aws.confluent.cloud:9092",
-    "security.protocol": "SASL_SSL",
-    "sasl.mechanisms": "PLAIN",
-    "sasl.username": "6WKBOV6NJHIKHNRZ",
-    "sasl.password": "9COXXRGSabZJlTTbogD6F3tbJ9uateQj1X53eQrE17DgiV59LifKHY7zLmoCSpxJ",
-}
