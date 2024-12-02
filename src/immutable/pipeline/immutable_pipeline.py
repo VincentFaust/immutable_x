48497d5d58b3f5ef753df5245177a7ad333238fc
Vincent Faust <vincentffaust@gmail.com>
2024-12-01 18:22:59 -0600

    push to csvs and create analysis directory

--- src/immutable/pipeline/immutable_pipeline.py
+++ src/immutable/pipeline/immutable_pipeline.py
@@ -3,6 +3,7 @@ from extract.orders import Order
 from extract.transfers import Transfer
 
 import logging
+import pandas as pd
 
 
 def pipeline():
@@ -50,9 +51,22 @@ def pipeline():
 
     logging.info("transfers endpoint extraction complete")
 
-    print(gods_orders.orders)
-    print(gods_mints.mints)
-    print(gods_transfers.transfers)
+    logging.info("converting lists to pandas dataframe start")
+    orders_df = pd.DataFrame(gods_orders.orders)
+    mints_df = pd.DataFrame(gods_mints.mints)
+    transfers_df = pd.DataFrame(gods_transfers.transfers)
+
+    logging.info("converting lists to pandas dataframe end")
+
+    output_directory = "/Users/fitz/Coding/immutable_x/src/immutable/analysis"
+
+    logging.info("creating csv start")
+
+    orders_df.to_csv(f"{output_directory}/orders.csv", index=False)
+    mints_df.to_csv(f"{output_directory}/mints.csv", index=False)
+    transfers_df.to_csv(f"{output_directory}/transfers.csv", index=False)
+
+    logging.info("creating csv end")
 
 
 if __name__ == "__main__":
